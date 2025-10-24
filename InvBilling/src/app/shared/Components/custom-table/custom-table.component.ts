eac10eab986fa62e6b789cf5b41851e38d82ecda
nammasocials <nammasocials.in@gmail.com>
2025-10-24 19:18:19 +0530

    Custom table Changes

--- InvBilling/src/app/shared/Components/custom-table/custom-table.component.ts
+++ InvBilling/src/app/shared/Components/custom-table/custom-table.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { customTableHeader } from '../../Models/custom-table';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './custom-table.component.html',
   styleUrl: './custom-table.component.css'
 })
@@ -13,8 +14,29 @@ export class CustomTableComponent {
 
   @Input() data: any[] = [];
   @Input() headerData: customTableHeader[] = [];
+  @Input() title: string = "Records";
+  noOfRecords : number = 5;
+
+  dataToDisplay: any[] = [];
+  searchTerm: string = '';
 
   constructor() {
+    this.dataToDisplay = this.data.slice(0, this.noOfRecords);
+  }
+  filterTable() {
+    if (this.searchTerm.length > 0) {
+      const lowerKeyword = this.searchTerm.toLowerCase();
+      this.dataToDisplay = this.data.filter(item =>
+        Object.values(item).some(value =>
+          String(value).toLowerCase().includes(lowerKeyword)
+        )
+      );
+    }
+    else {
+      this.dataToDisplay = this.data;
+    }
+  }
+  changeRowsPerPage(){
 
   }
 }
