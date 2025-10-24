c7fd938aa428a3dca15fdbd938621d736db6f18b
nammasocials <nammasocials.in@gmail.com>
2025-10-24 08:09:15 +0530

    SA - Loading Spinner Widjet

--- InvBilling/src/app/constants.ts
+++ InvBilling/src/app/constants.ts
@@ -1,4 +1,5 @@
 export class Constant{
     static readonly apiName: string = "KrgWebAPI";
     static readonly LogoSrc: string = "assets/images/Logo.png";
+    static readonly LoadingSrc: string = "assets/images/spinner.gif";
 }
--- InvBilling/src/app/shared/Components/loading-spinner/loading-spinner.component.ts
+++ InvBilling/src/app/shared/Components/loading-spinner/loading-spinner.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { LoadingService } from '../../Service/loading-service.service';
+import { Constant } from '../../../constants';
 
 @Component({
   selector: 'app-loading-spinner',
+  standalone:true,
   imports: [CommonModule],
   templateUrl: './loading-spinner.component.html',
   styleUrl: './loading-spinner.component.css'
 })
 export class LoadingSpinnerComponent {
+  readonly Constant = Constant;
   loading$ = inject(LoadingService).loading$;
 }
