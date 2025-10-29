66889894c05badadaa9e39cb3ca52245d680bd04
S Anand <Anand@fusionintek.com>
2025-10-29 15:13:27 +0530

    integrating mat table in custom table with paginator

--- InvBilling/src/app/shared/Components/custom-table/custom-table.component.ts
+++ InvBilling/src/app/shared/Components/custom-table/custom-table.component.ts
@@ -1,117 +1,118 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, SimpleChanges, ViewChild } from '@angular/core';
 import { customTableHeader } from '../../Models/custom-table';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-custom-table',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatFormFieldModule, // ✅ Add this
+    MatInputModule      // ✅ Add this
+  ],
   templateUrl: './custom-table.component.html',
   styleUrl: './custom-table.component.css'
 })
-export class CustomTableComponent {
-
+export class CustomTableComponent implements AfterViewInit {
   @Input() data: any[] = [];
-  tempData: any[] = [];
-  pageWiseData: any[] = [];
   @Input() headerData: customTableHeader[] = [];
   @Input() title: string = "Records";
+  @ViewChild('overlayHost', { static: true }) overlayHost!: ElementRef;
 
-  defaultItemsPerPageArray: number[] = [5, 10, 15, 20, 30];
-  itemsPerPageArray: number[] = [5, 10, 15, 20, 30];
-  itemsPerPage: number = 5;
-  defaultItemsPerPage: number = 5;
-  defaultCurrentPage: number = 1;
-  currentPage: number = 1;
+  pageSize = 5;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 20];
+  
+  startRecord : number = 0;
+  endRecord : number = 0;
 
-  dataToDisplay: any[][] = [];
-  searchTerm: string = '';
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  elementData = ELEMENT_DATA;
 
-  constructor() {
-    this.itemsPerPage = this.itemsPerPageArray[0];
-    this.renderTable();
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  ngAfterViewInit() {
+    this.updatePagedData();
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['data'] && changes['data'].currentValue) {
-      this.renderTable();
+
+  updatePagedData() {
+    const startIndex = this.pageIndex * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.dataSource.data = this.elementData.slice(startIndex, endIndex);
+    this.getRangeLabel();
+  }
+
+
+  onPageSizeChange(event: Event) {
+    const newSize = +(event.target as HTMLSelectElement).value;
+    this.pageSize = newSize;
+    this.pageIndex = 0; // reset to first page
+    this.updatePagedData();
+  }
+
+
+  nextPage() {
+    if ((this.pageIndex + 1) * this.pageSize < this.elementData.length) {
+      this.pageIndex++;
+      this.updatePagedData();
     }
   }
 
 
-  renderTable() {
-    ///// Step 01 Filter with serach term
-    this.tempData = this.data;
-    this.dataToDisplay = [];
-    this.filterTable();
-    this.calculateItemsPerPage();
-    this.calculatePagination();
-  }
-
-
-  filterTable() {
-    if (this.searchTerm.length > 0) {
-      const lowerKeyword = this.searchTerm.toLowerCase();
-      this.tempData = this.data.filter(item =>
-        Object.values(item).some(value =>
-          String(value).toLowerCase().includes(lowerKeyword)
-        )
-      );
-    }
-    else {
-      this.tempData = this.data;
-    }
-  }
-  calculateItemsPerPage() {
-    const length = this.tempData.length;
-    let defaultItemsPerPageArray = this.defaultItemsPerPageArray.filter(x => x >= length);
-    const nextMaxValArray = defaultItemsPerPageArray.filter(x => x >= length);
-
-    let nextMaxVal: number;
-
-    // If no number in array is greater or equal, assign max value from array
-    if (nextMaxValArray.length === 0) {
-      nextMaxVal = Math.max(...defaultItemsPerPageArray);
-    } else {
-      nextMaxVal = Math.min(...nextMaxValArray);
-    }
-    defaultItemsPerPageArray = this.defaultItemsPerPageArray;
-    const index = defaultItemsPerPageArray.indexOf(nextMaxVal);
-    this.itemsPerPageArray = defaultItemsPerPageArray.slice(0, index + 1);
-    
-  }
-  calculatePagination() {
-    const tempData = this.tempData; // array with records
-    const selectedPageSize = this.itemsPerPage;    // for example, selected pagination size (must be from defaultItemsPerPageArray)
-
-    this.dataToDisplay = [];
-
-    const totalRecords = tempData.length;
-    const totalPages = Math.ceil(totalRecords / selectedPageSize);
-    let startIndex = 0;
-    for (let page = 1; page <= totalPages; page++) {
-      let endIndex = Number(selectedPageSize) - Number(startIndex);
-      if(Number(startIndex) + Number(selectedPageSize) <=  totalRecords){
-        endIndex = Number(startIndex) + Number(selectedPageSize);
-      }
-      this.dataToDisplay[page] = tempData.slice(startIndex, endIndex);
-      startIndex = startIndex + selectedPageSize;
-    }
-    console.log(this.dataToDisplay);
-  }
-
-
-  changeRowsPerPage() {
-    this.renderTable();
-  }
-  changePage(isPrev : boolean){
-    if(isPrev){
-      this.currentPage = this.currentPage - 1;
-    }
-    else{
-      this.currentPage = this.currentPage + 1;
+  prevPage() {
+    if (this.pageIndex > 0) {
+      this.pageIndex--;
+      this.updatePagedData();
     }
   }
 
+
+  getRangeLabel() {
+    const start = this.pageIndex * this.pageSize + 1;
+    const end = Math.min(
+      (this.pageIndex + 1) * this.pageSize,
+      this.elementData.length
+    );
+    this.startRecord = start;
+    this.endRecord = end;
+  }
 }
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
+  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
+  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
+  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
+  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
+  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
+  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
+  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
+  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
+  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+];
--- InvBilling/src/main.ts
+++ InvBilling/src/main.ts
@@ -4,26 +4,16 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { APP_CONFIG } from './app/tokens/app-config.token';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 
 bootstrapApplication(AppComponent, {
   providers: [
+    importProvidersFrom(BrowserAnimationsModule),
     provideRouter(routes),
-    ...appConfig.providers // merge your app config providers if needed
+    ...appConfig.providers
   ]
 }).catch(err => console.error(err));
-
-
-
-fetch('/assets/config.json')
-  .then(res => res.json())
-  .then(config => {
-    bootstrapApplication(AppComponent, {
-      ...appConfig,
-      providers: [
-        ...appConfig.providers,
-        { provide: APP_CONFIG, useValue: config }
-      ]
-    });
-  });
 
 
