1649a15ee229496d6aaed76bc0572c031cb13346
Arun Gupta <arunofficia2023@gmail.com>
2026-02-17 18:33:28 +0530

    Refactored GST calculation and standardized UnitCost usage in InvoiceService

--- InvBilling/src/app/AuthModule/Models/AuthModels.ts
+++ InvBilling/src/app/AuthModule/Models/AuthModels.ts
@@ -3,9 +3,18 @@ export class VMAuthReq
     username : string = "";
     password : string = "";
 }
+
 export class VMAuthResponse
 {
     userCode : number = 0;
     token : string = "";
-    isAuthenticated : boolean = true
- }
+    isAuthenticated : boolean = true;
+    forcePasswordChange : boolean = false;
+}
+
+export class VMPasswordChangeReq
+{
+    oldPassword : string = "";
+    newPassword : string = "";
+    confirmPassword : string = "";
+}
